c38835d53891d5c54ced41f085f41aafa9d0d9fe
josefarnes <twokadoz@galileo.edu>
2020-03-03 12:48:46 -0600

    task queue implemented

--- diskScheduler.c
+++ diskScheduler.c
@@ -1,9 +1,64 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-void fillTheProcessArray(){
+#define MAX 100
 
-   
+int intArray[MAX];
+int front = 0;
+int rear = -1;
+int itemCount = 0;
+
+int peek() {
+   return intArray[front];
+}
+
+bool isEmpty() {
+   return itemCount == 0;
+}
+
+bool isFull() {
+   return itemCount == MAX;
+}
+
+int size() {
+   return itemCount;
+}  
+
+void insert(int data) {
+
+   if(!isFull()) {
+	
+      if(rear == MAX-1) {
+         rear = -1;            
+      }       
+
+      intArray[++rear] = data;
+      itemCount++;
+   }
+}
+
+int removeData() {
+   int data = intArray[front++];
+	
+   if(front == MAX) {
+      front = 0;
+   }
+	
+   itemCount--;
+   return data;  
+}
+
+void fillTheProcessArray(int taskDelimiter){
+   int inputTask;
+
+   for(int i = 0; i < taskDelimiter; i++){
+      printf("Input the current task \n");
+      scanf("%d", &inputTask);
+      insert(inputTask);
+
+   }
 }
 
 int main( ) {
@@ -31,7 +86,7 @@ int main( ) {
    if(numberOfTasks < 0 || numberOfTasks > 100){
       printf("Number of block must be whithin [1-100].");
    } else if (numberOfTasks < 100 && numberOfTasks > 0)
-      fillTheProcessArray();
+      fillTheProcessArray(numberOfTasks);
 
    /*requesting in what block should the head start*/
    printf("Where should the head start?:\n");
